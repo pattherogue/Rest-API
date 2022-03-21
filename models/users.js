b9810c76dd8c67b843000e1f40a984efa69c1508
Paolo Gomez <peweegardner248@gmail.com>
2022-03-21 11:14:43 -0700

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,12 +1,23 @@
 'use strict';
 
-const { Model } = require('sequelize');
-const { INSERT } = require('sequelize/types/query-types');
+const { Model, DataTypes } = require('sequelize');
 
 module.exports = (sequelize) =>  {
     class Users extends Model {}
     
     Users.init({
-        
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A FIRST NAME is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a FIRST NAME'
+                }
+            }
+        },
+
     })
 }
