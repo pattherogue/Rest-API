e482eec2e302ef6be80a1e2127ff60258017eb50
Paolo Gomez <peweegardner248@gmail.com>
2022-03-21 13:26:03 -0700

    Update courses.js

--- models/courses.js
+++ models/courses.js
@@ -1,3 +1,33 @@
 'use strict';
 
 const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Courses extends Model {}
+    Courses.init({
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A title is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a title'
+                }
+            }
+        },
+
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A description is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a description'
+                }
+            }
+        },
+}
