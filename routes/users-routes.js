a057230b67b6a9ac167718abc61779fb8c94736f
Paolo Gomez <peweegardner248@gmail.com>
2022-03-23 09:07:01 -0700

    Update users-routes.js

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,3 +2,9 @@
 /* return propertires and values */
 /* currently authenticated user */
 /* 200 HTTP status code */
+
+/* POST route */
+/* create new user */
+/* location header to "/" */
+/* return 201 http status code */
+/* no content */
