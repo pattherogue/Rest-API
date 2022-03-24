c83f5f7b15025a47cbfe10ee7f9ffd60f64e3ac8
Paolo Gomez <peweegardner248@gmail.com>
2022-03-24 12:10:58 -0700

    Update courses-routes.js

--- routes/courses-routes.js
+++ routes/courses-routes.js
@@ -6,4 +6,9 @@
 /* /api/courses/:id GET route */
 /* return corresponding course */
 /* include user associated with course */
-/* return 200 HTTP status code */
+/* return 200 HTTP status code and no content */
+
+/* /api/courses POST route */
+/* create new course */
+/* set Location header to URl new course */
+/* return 204 HTTP status code and no content */
