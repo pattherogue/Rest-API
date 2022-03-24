26186d55247c0b76c245323a949e5d34bcb455d9
Paolo Gomez <peweegardner248@gmail.com>
2022-03-24 12:09:30 -0700

    Update courses-routes.js

--- routes/courses-routes.js
+++ routes/courses-routes.js
@@ -1,6 +1,9 @@
 /* /api/courses GET route */
 /* retunrn all courses */
 /* User associated with course */
-/* 200 HTTP status code */
+/*  return 200 HTTP status code */
 
 /* /api/courses/:id GET route */
+/* return corresponding course */
+/* include user associated with course */
+/* return 200 HTTP status code */
