8801a092d4f1ba8cb841ced01d884b04671388e8
Paolo Gomez <peweegardner248@gmail.com>
2022-03-28 12:01:45 -0700

    Update users-routes.js

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { User } = require('./models');
 const { authenticateUser } = require('./middleware/auth-user');
+const { Router } = require('express');
+const { asyncHandler } = require('../middleware/asyncHandler');
 
 /* GET route */
 /* return all  propertires and values */
 /* currently authenticated user */
 /* 200 HTTP status code */
+
+// Route that returns the current authenticated user 
+Router.get('/users', asyncHandler(async (req, res) => {
+
+}));
 
 /* POST route */
 /* create new user */
