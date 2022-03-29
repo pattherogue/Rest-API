7b2d46d1bb26d276f6e2ac13f4300acae296666a
Paolo Gomez <peweegardner248@gmail.com>
2022-03-29 11:42:37 -0700

    Update courses-routes.js

--- routes/courses-routes.js
+++ routes/courses-routes.js
@@ -6,11 +6,15 @@ const { authenticateUser } = require('../middleware/auth-user');
 const { asyncHandler } = require('../middleware/asyncHandler');
 const { User } = require('../models');
 const { Course } = require('../models');
+
 /* /api/courses GET route */
 /* retunrn all courses */
 /* User associated with course */
 /*  return 200 HTTP status code */
 
+router.get('/courses', asyncHandler (async (req, res) => {
+
+}));
 /* /api/courses/:id GET route */
 /* return corresponding course */
 /* include user associated with course */
