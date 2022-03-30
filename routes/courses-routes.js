bf427d5dc6f0754b194b617b3ba4768771738c94
Paolo Gomez <peweegardner248@gmail.com>
2022-03-30 11:53:47 -0700

    Update courses-routes.js

--- routes/courses-routes.js
+++ routes/courses-routes.js
@@ -21,7 +21,7 @@ router.get('/courses', asyncHandler (async (req, res) => {
     if (course) {
         res.status(200).json(course);
     } else {
-        res.status(404)
+        res.status(404).json({ message: "Unable to find course."})
     }
 }));
 /* /api/courses/:id GET route */
