d4e9e7000041b3e0fe8fa7f7bc33825b1df916d3
Paolo Gomez <peweegardner248@gmail.com>
2022-04-11 15:21:03 -0700

    Update auth-user.js

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -12,17 +12,17 @@ exports.authenticateUser = async (req, res, next) => {
     const credentials = auth(req);
 
     if (credentials) {
-        const user = await User.findOne({ where: {emailAdress: credentials.name} });
+        const user = await User.findOne({ where: {emailAddress: credentials.name} });
         if (user) {
             const authenticated = bcrypt
                 .compareSync(credentials.pass, user.password);
             if (authenticated) { // If the passwords match 
-                console.log(`Authentication successful for username: ${user.username}`);
+                console.log(`Authentication successful for username: ${user.emailAddress}`);
                 
                 // Store the user on the Request Object.
                 req.currentUser = user;
             } else {
-                message = `Authentication failure for username: ${user.username}`;
+                message = `Authentication failure for username: ${user.emailAddress}`;
             }
         } else {
             message = `User not found for username: ${credentials.name}`;
