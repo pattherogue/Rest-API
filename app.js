e6af65c2286bc4d85fece9c0e2ec9375b1ff0e5c
Paolo Gomez <peweegardner248@gmail.com>
2022-04-11 15:38:53 -0700

    update

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const morgan = require('morgan');
 const sequelize = require('./models').sequelize;
 // const { Sequelize } = require('sequelize/types');
 
-const userRoutes = require('./routes/users-routes');
-const courseRoutes = require('./routes/courses-routes');
+const userRoutes = require('./routes/user-routes');
+const courseRoutes = require('./routes/course-routes');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
